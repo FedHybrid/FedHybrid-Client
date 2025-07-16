5404d67742f9d06017ff7f201fd5bcf47fa7fb83
kimsuhyeon <suhyun10201@naver.com>
2025-07-16 15:18:44 +0900

    add/#16: 생성 페이지 파일 추가

---
+++ src/app/instance/create/page.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import styled from "styled-components";
+
+export default function InstanceCreate() {
+  return (
+    <Container>
+      <Content>
+        <Title>인스턴스 생성</Title>
+      </Content>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 2rem 2rem;
+  background-color: #f9fafb;
+  `
+
+const Content = styled.div`
+  width: 100%;
+  margin: 0 auto;
+`
+
+const Title = styled.h1`
+  font-size: 2.25rem;
+  font-weight: 700;
+  color: #111827;
+  text-align: center;
+  margin-bottom: 3rem;
+`
